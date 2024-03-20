fb1221e58e293b5a7f18a688548313767870f2f9
RAHUL94827 <rahul2223rrs@gmail.com>
2024-03-20 18:42:12 +0530

    Level 3 using Hash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express=require('express');
 const ejs=require('ejs');
 const bodyParser=require('body-parser')
 const mongoose=require('mongoose')
-const encrypt=require('mongoose-encryption')
+const md5=require('md5');
 
 const app=express();
 
@@ -20,7 +20,6 @@ const userSchema=new mongoose.Schema({
 })
 
 
-userSchema.plugin(encrypt, { secret:process.env.SECRET,encryptedFields:["password"] });
 
 
 const User=new mongoose.model("User",userSchema);
@@ -42,7 +41,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save()
     res.render("secrets")
@@ -51,7 +50,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",async function(req,res){
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     const login_result=await User.findOne({email:username})
     if(login_result){
